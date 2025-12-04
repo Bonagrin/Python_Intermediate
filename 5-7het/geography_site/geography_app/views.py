40c0d17f2f20373db692e458ec284edf11e5343e
Bonagrin <matelorik1@gmail.com>
2025-12-04 18:09:09 +0100

    Modify src files and add new html files for dynamic actions handling between frontend backend

--- 5-7het/geography_site/geography_app/views.py
+++ 5-7het/geography_site/geography_app/views.py
@@ -1,39 +1,80 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.template import loader
+from django.urls import reverse
 from geography_app.models import Country, City
 
+
 def index(request):
+    cities = City.objects.all()
     return HttpResponse(loader.get_template('index.html').render({
         'countries': Country.objects.all(),
-        'cities': City.objects.all(),
+        'cities': cities,
+        'eu_capitals': [city.name for city in cities if city.is_capital and city.country.eu_member],
     }, request))
 
+
 def add_country(request):
-    return HttpResponse('add_country')
+    return HttpResponse(loader.get_template('country.html').render({}, request))
+
 
 def add_country_record(request):
-    return HttpResponse('add_country_record')
+    Country(
+        name=request.POST.get('name'),
+        eu_member=request.POST.get('eu_member') == 'on'
+    ).save()
+    return HttpResponseRedirect(reverse('index'))
+
 
 def update_country(request, id):
-    return HttpResponse(f'Update_country: {id}')
+    return HttpResponse(loader.get_template('country.html').render({
+        'country': Country.objects.get(id=id)
+    }, request))
+
 
 def update_country_record(request, id):
-    return HttpResponse(f'Update_country_record: {id}')
+    country = Country.objects.get(id=id)
+    country.name = request.POST.get('name')
+    country.eu_member = request.POST.get('eu_member') == 'on'
+    country.save()
+    return HttpResponseRedirect(reverse('index'))
+
 
 def delete_country(request, id):
-    return HttpResponse(f'Delete_country: {id}')
+    Country.objects.get(id=id).delete()
+    return HttpResponseRedirect(reverse('index'))
+
 
 def add_city(request):
-    return HttpResponse('add_city')
+    return HttpResponse(loader.get_template('city.html').render({
+        'countries': Country.objects.all(),
+    }, request))
+
 
 def add_city_record(request):
-    return HttpResponse('add_city_record')
+    City(
+        name=request.POST.get('name'),
+        is_capital=request.POST.get('is_capital') == 'on',
+        country=Country.objects.get(id=request.POST.get('country_id'))
+    ).save()
+    return HttpResponseRedirect(reverse('index'))
+
 
 def update_city(request, id):
-    return HttpResponse(f'Update_city: {id}')
+    return HttpResponse(loader.get_template('city.html').render({
+        'city': City.objects.get(id=id),
+        'countries': Country.objects.all(),
+    }, request))
+
 
 def update_city_record(request, id):
-    return HttpResponse(f'Update_city_record: {id}')
+    city = City.objects.get(id=id)
+    city.name = request.POST.get('name')
+    city.is_capital = request.POST.get('is_capital') == 'on'
+    city.country = Country.objects.get(id=request.POST.get('country_id'))
+    city.save()
+    return HttpResponseRedirect(reverse('index'))
+
 
 def delete_city(request, id):
-    return HttpResponse(f'Delete_city: {id}')
+    City.objects.get(id=id).delete()
+    return HttpResponseRedirect(reverse('index'))
